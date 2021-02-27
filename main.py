6dd067451c6fde1c79aeaff7e136c2ae9507afab
Denys Murakhovskyi <dmurakhowsky@gmail.com>
2021-02-28 01:34:43 +0200

    Parallel/Serial processes run

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+# импорт элементов GUI
 from tkinter import Tk, Label, Frame, LabelFrame, Button, LEFT, TOP, Radiobutton
-from tkinter import BooleanVar, W, IntVar
+from tkinter import BooleanVar, W, IntVar, DISABLED
 from tkinter import messagebox as mb
 
+# импорт прочих библиотек
 from time import sleep
 import sys
 from random import randint
 
-from multiprocessing import Process, Queue
+# импорт библиотеки для работы с несколькими процессами
+from multiprocessing import Process, Queue, Event
 
 
 class App:
+    """
+    класс, отвечающий за GUI и его основной поток
+    """
 
     def __init__(self, main_thread, q, process):
-        # ui creation
+        # создание GUI
         self.window = main_thread
         self.window.geometry('210x260')
         self.window.title('Threads')
@@ -32,7 +38,8 @@ class App:
         Radiobutton(radio, text='Parallel', variable=self.launch_type, value=1, width=22, anchor=W).pack(side=TOP)
         buttons = LabelFrame(self.window, text='')
         subbuttons = Frame(buttons)
-        Button(subbuttons, text='Start', width=10, command=self.start).pack(side=LEFT, padx=(10, 5), pady=5)
+        self.startButton = Button(subbuttons, text='Start', width=10, command=self.start)
+        self.startButton.pack(side=LEFT, padx=(10, 5), pady=5)
         Button(subbuttons, text='Stop', width=10, command=self.stop).pack(side=LEFT, padx=(0, 5), pady=5)
         subbuttons.pack(side=TOP)
         Button(buttons, text='Exit', width=22, command=lambda x=0: sys.exit(x)).pack(side=TOP, padx=(10, 5), pady=5)
@@ -40,37 +47,83 @@ class App:
         radio.pack(side=TOP, padx=5, pady=(0, 5))
         buttons.pack(side=TOP, padx=5, pady=(5, 10))
 
-        self.q = q
-        self.updaters = process
+        # создание элементов для многопроцессности и обмена данными между потоками
+        self.q = q  # очередь, используется для обмена элементами
+        self.updaters = process  # процессы, которые буду запущены
+        self.ran = [False, False]
 
     def periodicCall(self):
+        """
+        Метод, проверяющие периодически наличие элементов в очереди.
+        Без непосредственно обновления элементов GUI - единственный способ взаимодействия с очередью
+        :return:
+        """
+        # чтение из очереди
         if not self.q.empty():
             a = self.q.get_nowait()
             self.labels[a[0]].set(a[1])
+
+        """ обработка последовательного запуска потоков
+        если первый поток завершил работу - запускается второй """
+        if (not self.launch_type.get()) & (not(self.updaters[0].is_alive())) & (not(self.updaters[1].is_alive())):
+            if not self.ran[1]:
+                self.updaters[1].start()
+                self.ran[1] = True
+
+        """ рекурсивный вызов обработчика
+        совершается только если один из потоков не завершил работу """
         if self.updaters[0].is_alive() | self.updaters[1].is_alive():
             self.window.after(200, self.periodicCall)
 
     def start(self):
-        for item in self.updaters:
-            try:
-                item.start()
-            except AssertionError:
-                mb.showerror(title='Error', message='Already running')
-                break
+        """
+        обработчик кнопки Start
+        :return:
+        """
+        self.startButton.config(state=DISABLED)  # потоки запускаются только один раз. Кнопка становится недоступной
+        if self.launch_type.get():
+            # запуск потоков параллельно
+            for item in zip(self.updaters, self.ran):
+                try:
+                    item[0].start()
+                    item[1] = True
+                except AssertionError:
+                    mb.showerror(title='Error', message='Already running')
+                    break
+        else:
+            # запуск потоков последовательно
+            self.updaters[0].start()
+            self.ran[0] = True
+        # запуск обработчика очереди первый раз после запуска потоков
         self.window.after(200, self.periodicCall)
 
     def stop(self):
+        """
+        Обработчик кнопки Stop
+        Останавливает - убивает потоки
+        :return:
+        """
         for item in self.updaters:
-            item.kill()
+            try:
+                item.kill()
+            except:
+                pass
 
 
 class Calc:
-
+    """
+    Рабочий класс для произведения вычислений и выдачи результатов в очередь
+    """
     def __init__(self, N, q):
         self.itemID = N
         self.q = q
 
     def worker(self):
+        """
+        Основной расчетный метод
+        Генерирует случайное число и помещает его в очередь
+        :return:
+        """
         for i in range(11):
             a = randint(0 + 10 * self.itemID, 11 + 10 * self.itemID)
             self.q.put((self.itemID, a))
@@ -78,21 +131,31 @@ class Calc:
 
 
 class Threaded:
+    """
+    Основной класс приложения управления потоками
+    Создает и класс GUI и экземпляры классов расчетов
+    """
     def __init__(self, main_thread):
+        # основной поток - поток GUI
         self.main_thread = main_thread
 
+        # очередь для обмена данными между потоками
         self.q = Queue()
 
+        # создание экзепмляров класса, предназначенного для расчетов
         w1 = Calc(0, self.q)
         w2 = Calc(1, self.q)
 
+        # создание потоков для выполнения расчетов
         self.updaters = [Process(target=w1.worker, args=()),
                          Process(target=w2.worker, args=())]
 
+        # создание экземпляра класса GUI
         self.gui = App(main_thread, self.q, self.updaters)
 
 
 def main():
+    # создание объекта TKinter и запуск приложения
     main_thread = Tk()
     Threaded(main_thread)
     main_thread.mainloop()
